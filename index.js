a588b4a7c71581f58e002868eda8903a6bdc7f3e
mblackstock <mikeblackstock@gmail.com>
2018-04-04 17:53:41 -0700

    change project names

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const sub1 = require('sub1');
+const sub1 = require('@mblackstock/test-sub1');
+const sub2 = require('@mblackstock/test-sub2');
 
 console.log('running root project');
 console.log(sub1.foo());
+console.log(sub2())
--- packages/sub1/index.js
+++
@@ -1,9 +0,0 @@
-
-const sub2 = require('sub2');
-
-module.exports = {
-  foo: function() {
-    console.log('sub1 called');
-    sub2();
-  }
-}
